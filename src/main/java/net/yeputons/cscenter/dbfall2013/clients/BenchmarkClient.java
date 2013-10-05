9bc617386d7446a8391261807352723a46a3798b
yeputons <egor.suvorov@gmail.com>
2013-10-05 20:02:16 +0400

    BenchmarkClient was added: it just measures average speed of an engine (puts per second)

---
+++ src/main/java/net/yeputons/cscenter/dbfall2013/clients/BenchmarkClient.java
@@ -0,0 +1,45 @@
+package net.yeputons.cscenter.dbfall2013.clients;
+
+import net.yeputons.cscenter.dbfall2013.engines.LogFileEngine;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.util.Random;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: e.suvorov
+ * Date: 05.10.13
+ * Time: 19:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class BenchmarkClient {
+    public static void main(String[] args) throws Exception {
+        File f = File.createTempFile("benchmark", ".log");
+        f.delete();
+
+        final LogFileEngine engine = new LogFileEngine(f);
+
+        long startTime = System.currentTimeMillis();
+        byte[] key = new byte[10];
+        byte[] value = new byte[10];
+        Random rand = new Random();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                engine.close();
+            }
+        }));
+        for (int step = 1;; step++) {
+            rand.nextBytes(key);
+            rand.nextBytes(value);
+            engine.put(ByteBuffer.wrap(key), ByteBuffer.wrap(value));
+            if (step % 300 == 0) {
+                engine.flush();
+                double elapsed = (double)(System.currentTimeMillis() - startTime) / 1000.0;
+                System.out.printf("size=%d, elapsed=%.2f, puts per second=%.2f\n", engine.size(), elapsed, (double)engine.size() / elapsed);
+            }
+        }
+    }
+}
