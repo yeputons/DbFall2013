320657aa805835fe72d752fd15ed35a191404eb9
yeputons <egor.suvorov@gmail.com>
2013-10-13 20:11:21 +0400

    ShardDescription: hashCode() and equals() methods were added

--- src/main/java/net/yeputons/cscenter/dbfall2013/scaling/ShardDescription.java
+++ src/main/java/net/yeputons/cscenter/dbfall2013/scaling/ShardDescription.java
@@ -10,4 +10,19 @@ public class ShardDescription {
     public Socket openSocket() throws IOException {
         return new Socket(host, port);
     }
+
+    @Override
+    public int hashCode() {
+        return host.hashCode() * 239017 + port;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        if (!(obj instanceof ShardDescription))
+            return false;
+        ShardDescription desc = (ShardDescription)obj;
+        return host.equals(desc.host) && port == desc.port;
+    }
 }
