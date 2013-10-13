31f9a447509439a557188d13fc534fd2a46d2f3d
yeputons <egor.suvorov@gmail.com>
2013-10-13 20:11:44 +0400

    scaling.Router: it now caches active connections to nodes and don't close them

--- src/main/java/net/yeputons/cscenter/dbfall2013/scaling/Router.java
+++ src/main/java/net/yeputons/cscenter/dbfall2013/scaling/Router.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -17,33 +19,59 @@ import java.util.Set;
  * Time: 19:50
  * To change this template use File | Settings | File Templates.
  */
-public class Router extends SimpleEngine {
+public class Router extends SimpleEngine implements AutoCloseable {
+    protected class ShardConnection {
+        public Socket s;
+        public DataInputStream in;
+        public DataOutputStream out;
+
+        public ShardConnection(Socket s) throws IOException {
+            this.s = s;
+            in = new DataInputStream(s.getInputStream());
+            out = new DataOutputStream(s.getOutputStream());
+        }
+        public void readOk() throws IOException {
+            byte[] res = new byte[2];
+            in.readFully(res);
+            if (!Arrays.equals(res, "ok".getBytes())) {
+                throw new RuntimeException("Server returned an unknown status: " + new String(res));
+            }
+        }
+    }
+
     ShardingConfiguration conf;
+    HashMap<ShardDescription, ShardConnection> connections;
 
     public Router(ShardingConfiguration conf) {
         this.conf = conf;
+        connections = new HashMap<ShardDescription, ShardConnection>();
+    }
+
+    protected ShardConnection getConnection(ShardDescription shard) throws IOException {
+        ShardConnection conn = connections.get(shard);
+        if (conn == null) {
+            conn = new ShardConnection(shard.openSocket());
+            connections.put(shard, conn);
+        }
+        return conn;
+    }
+    protected ShardConnection getConnection(byte[] key) throws IOException {
+        return getConnection(conf.getShard(key));
     }
 
     @Override
     public ByteBuffer get(Object _key) {
         ByteBuffer key = (ByteBuffer)_key;
         try {
-            Socket s = conf.getShard(key.array()).openSocket();
-            DataInputStream in = new DataInputStream(s.getInputStream());
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            out.write("get".getBytes());
-            out.writeInt(key.limit());
-            out.write(key.array());
+            ShardConnection c = getConnection(key.array());
+            c.out.write("get".getBytes());
+            c.out.writeInt(key.limit());
+            c.out.write(key.array());
 
-            byte[] res = new byte[2];
-            in.readFully(res);
-            if (!Arrays.equals(res, "ok".getBytes())) {
-                throw new RuntimeException("Server returned an unknown status: " + new String(res));
-            }
-            int len = in.readInt();
+            c.readOk();
+            int len = c.in.readInt();
             byte[] value = new byte[len];
-            in.readFully(value);
-            s.close();
+            c.in.readFully(value);
 
             return ByteBuffer.wrap(value);
         } catch (IOException e) {
@@ -54,14 +82,13 @@ public class Router extends SimpleEngine {
     @Override
     public ByteBuffer put(ByteBuffer key, ByteBuffer value) {
         try {
-            Socket s = conf.getShard(key.array()).openSocket();
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            out.write("put".getBytes());
-            out.writeInt(value.limit());
-            out.write(value.array());
-            out.writeInt(key.limit());
-            out.write(key.array());
-            s.close();
+            ShardConnection c = getConnection(key.array());
+            c.out.write("put".getBytes());
+            c.out.writeInt(value.limit());
+            c.out.write(value.array());
+            c.out.writeInt(key.limit());
+            c.out.write(key.array());
+            c.readOk();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -72,12 +99,11 @@ public class Router extends SimpleEngine {
     public ByteBuffer remove(Object _key) {
         ByteBuffer key = (ByteBuffer)_key;
         try {
-            Socket s = conf.getShard(key.array()).openSocket();
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            out.write("del".getBytes());
-            out.writeInt(key.limit());
-            out.write(key.array());
-            s.close();
+            ShardConnection c = getConnection(key.array());
+            c.out.write("del".getBytes());
+            c.out.writeInt(key.limit());
+            c.out.write(key.array());
+            c.readOk();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -89,9 +115,9 @@ public class Router extends SimpleEngine {
         for (Entry<String, ShardDescription> shard : conf.shards.entrySet()) {
             Socket s = null;
             try {
-                s = shard.getValue().openSocket();
-                s.getOutputStream().write("clr".getBytes());
-                s.close();
+                ShardConnection c = getConnection(shard.getValue());
+                c.out.write("clr".getBytes());
+                c.readOk();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -106,5 +132,15 @@ public class Router extends SimpleEngine {
     @Override
     public Set<ByteBuffer> keySet() {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void close() throws Exception {
+        for (Entry<ShardDescription, ShardConnection> c : connections.entrySet()) {
+            c.getValue().in.close();
+            c.getValue().out.close();
+            c.getValue().s.close();
+        }
+        connections.clear();
     }
 }
