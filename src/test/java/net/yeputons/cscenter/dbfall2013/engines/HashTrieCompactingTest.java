cc32f3007b243ffb762eeb4e25d3029c292e3c71
Egor Suvorov <egor.suvorov@gmail.com>
2013-10-25 17:46:52 +0400

    HashTrieCompactingTest was added

---
+++ src/test/java/net/yeputons/cscenter/dbfall2013/engines/HashTrieCompactingTest.java
@@ -0,0 +1,65 @@
+package net.yeputons.cscenter.dbfall2013.engines;
+
+import net.yeputons.cscenter.dbfall2013.engines.hashtrie.HashTrieEngine;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Egor Suvorov
+ * Date: 25.10.13
+ * Time: 17:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class HashTrieCompactingTest extends AbstractStressTest {
+    final Logger log = LoggerFactory.getLogger(HashTrieCompactingTest.class);
+    protected File storage;
+
+    public HashTrieCompactingTest() throws IOException {
+        storage = File.createTempFile("storage", ".bin");
+        storage.delete();
+    }
+
+    @Test
+    public void stressTest() throws IOException {
+        final HashTrieEngine engine = new HashTrieEngine(this.storage);
+        Map<ByteBuffer, ByteBuffer> real = new HashMap<ByteBuffer, ByteBuffer>();
+        assertEquals(real, engine);
+
+        Random rnd = new Random();
+        for (int step = 0; step < 500; step++) {
+            if (rnd.nextInt(100) <= 1) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            engine.runCompaction();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (IllegalStateException e) {
+                        }
+                    }
+                }).start();
+            }
+            synchronized (engine) {
+                performRandomOperation(rnd, engine, real);
+
+                assertEquals(engine.size(), engine.keySet().size());
+                assertEquals(engine.size(), engine.entrySet().size());
+                assertEquals(real.keySet(), engine.keySet());
+                assertEquals(real.entrySet(), engine.entrySet());
+                assertEquals(real, engine);
+            }
+        }
+    }
+}
